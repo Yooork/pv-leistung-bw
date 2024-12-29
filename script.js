8bfbcad904957b3da872c19bb1b2048816ff5476
york <yorkkoch02@gmail.com>
2024-12-29 20:58:29 +0100

    added Abstufungen

---
+++ script.js
@@ -0,0 +1,129 @@
+var data;
+(async () => {
+     data = await getData();
+    console.log("Erhaltene Daten:", data?.PLZ_PV); // Auf PLZ_PV zugreifen
+})()
+
+
+setTimeout(() => {
+console.log(data);
+
+
+
+console.log(data.PLZ_PV);
+console.log(data.PLZ_PV[0].PLZ);
+
+
+
+var map = L.map('map').setView([48.791, 9.195], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.geoJSON(orte, {onEachFeature: onEachFeature, style: style}).addTo(map);
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'legend'),
+        grades = [ 0, 1900, 1920, 1940, 1960, 1980, 2000],
+        labels = [];
+
+    div.innerHTML += '<strong>Baujahr</strong><br>';
+    div.innerHTML += '<i style="background:black; display: inline-block; width: 18px; height: 18px;margin-right: 8px;border-radius: 3px;"></i> ' +
+    'undefined<br>';
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i]) + '; display: inline-block; width: 18px; height: 18px;margin-right: 8px;border-radius: 3px;"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
+}, 2000);
+
+function getColor(pv) {
+    var abstufungen=getAbstufung(6);
+    console.log(abstufungen);
+    return pv > abstufungen[5] ? '#00FF00' :
+    pv > abstufungen[4] ? '#80FF00' :
+    pv > abstufungen[3] ? '#FFFF00' :
+    pv > abstufungen[2] ? '#FFC000' : 
+    pv > abstufungen[1] ? '#FF8000' :
+    pv > 0 ? '#FF0000' : (() => { 
+        console.log('PV0:', pv); 
+        return '#000000'; 
+    })();
+
+}
+
+function style(feature) {
+    console.log(getPvPerPLZ(feature.properties.plz_code));
+    return {
+        fillColor: getColor(parseInt(getPvPerPLZ(feature.properties.plz_code))),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        fillOpacity: 0.95
+    }
+}
+
+function onEachFeature(feature, layer) {
+    layer.bindPopup(
+        '<h4>Year of construction: ' +  
+        feature.properties.plz_name + ' </h4>');
+}
+
+function getPvPerPLZ(plz){
+    for(let datax of data.PLZ_PV){
+        if(datax.PLZ==plz){
+            return datax.PV
+        }
+    }
+}
+
+function getAbstufung(numberOfAbstufung){
+    var min = data.PLZ_PV[0];
+    var max = 0;
+    for(let datax of data.PLZ_PV){
+        if(datax.PV>max){
+            max=datax.PV;
+            console.log(datax.PV,'dd')
+        }
+    } 
+
+    var abstufungen=[];
+
+    for(var i=0;i<=numberOfAbstufung;i++){
+        abstufungen[i]=(max/numberOfAbstufung)*i;
+    }
+    
+    return abstufungen;
+
+}
+
+
+async function getData() {
+    const url = "http://127.0.0.1:5000";
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP-Fehler! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log("Abgerufene Daten:", data);
+        return data; // Die JSON-Daten werden zurückgegeben
+    } catch (error) {
+        console.error("Fehler beim Abrufen der Daten:", error);
+        return null; // Gib im Fehlerfall `null` zurück
+    }
+}
+
+
+
